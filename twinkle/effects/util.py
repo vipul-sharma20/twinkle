07b8696e79b29702fb1ea469695d54237da55c9c
Vipul Sharma <vipul.sharma20@gmail.com>
2022-02-20 17:56:45 +0530

    add: wheel utility function

---
+++ twinkle/effects/util.py
@@ -0,0 +1,15 @@
+from rpi_ws281x import Color
+
+
+def wheel(pos: int) -> Color:
+    """
+    Generate rainbow colors across 0-255 positions.
+    """
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
